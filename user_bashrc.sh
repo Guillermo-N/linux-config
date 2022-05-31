e4fe3796b80e6ed005f713e3e92ee5154352fbe1
Guillermo-N <106453304+Guillermo-N@users.noreply.github.com>
2022-05-31 20:53:44 +0200

    Update user_bashrc.sh

--- user_bashrc.sh
+++ user_bashrc.sh
@@ -1,4 +1,26 @@
-#!/bin/bash
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# User specific environment
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]
+then
+    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+
+# Neofetch welcome print
+if [ -f /bin/neofetch ]; then
+	neofetch
+fi
 
 # Git status prompt
 if [ -f /usr/share/doc/git/contrib/completion/git-prompt.sh ]; then
